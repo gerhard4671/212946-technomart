92f49c8376921cee93c307d79febd4e664185a95
gerhard4671 <ostral64@yahoo.com>
2016-08-23 02:20:35 +0300

    эффекты для футера, сделал поп ап на карте, сверстал в нижнем слайдере все три слайдеа

---
+++ js/main.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+
+    var miniMap = document.getElementById("mini-map");
+    var modalMap = document.getElementById("modal-map");
+    var closeBtn = document.getElementById("close-map-btn");
+
+    miniMap.onclick = function () {
+        modalMap.style.display="block";
+    };
+
+    closeBtn.onclick = function () {
+        modalMap.style.display="none";
+    };
+};
