dfa25222d5b20d999b6c162a8efca1073bac36fa
gerhard4671 <ostral64@yahoo.com>
2016-08-24 20:57:59 +0300

    Главная полностью готова, без слайдеров

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ window.onload = function () {
     var miniMap = document.getElementById("mini-map");
     var modalMap = document.getElementById("modal-map");
     var closeBtn = document.getElementById("close-map-btn");
+    var contactForm = document.getElementById("contact-form");
+    var closeForm = document.getElementById("close-form-btn");
+    var modalForm = document.getElementById("modal-form");
+
 
     miniMap.onclick = function () {
         modalMap.style.display="block";
@@ -10,6 +14,13 @@ window.onload = function () {
 
     closeBtn.onclick = function () {
         modalMap.style.display="none";
+    };
+
+    contactForm.onclick = function () {
+        modalForm.style.display="block";
+    };
+    closeForm.onclick = function () {
+        modalForm.style.display="none";
     };
 
 };
